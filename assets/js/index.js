c161c7484d003c06fb8f7e3118df3999e9269b0b
zero-dev <andrianow817@gmail.com>
2026-01-14 13:49:28 +0700

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("../../assets/js/sw.js")
+    .register("../../sw.js")
     .then(() => console.log("Service Worker Registered"))
     .catch((err) => console.error("SW Registration Failed", err));
 }
