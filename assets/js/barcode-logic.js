be98f7890da613b1cd515c05d0602695e3f4ebf5
zero-dev <andrianow817@gmail.com>
2026-01-14 14:42:15 +0700

    test

--- assets/js/barcode-logic.js
+++ assets/js/barcode-logic.js
@@ -68,12 +68,28 @@ const onScanSuccess = (decodedText) => {
 };
 
 function saveImportedData(dataList) {
+  console.log(dataList);
+
   const transaction = db.transaction(["jadwal_store"], "readwrite");
   const store = transaction.objectStore("jadwal_store");
 
-  dataList.forEach((item) => {
-    item.updatedAt = new Date().toISOString();
-    item.isAlarmActive = true;
+  dataList.forEach((row) => {
+    // KITA BALIKKAN DARI ARRAY KE OBJECT DI SINI
+    // Urutan harus sama dengan saat generate: [matkul, hari, semester, jamMulai, jamSelesai, ruang]
+    const item = {
+      matkul: row[0],
+      hari: row[1],
+      semester: row[2],
+      jamMulai: row[3],
+      jamSelesai: row[4],
+      ruang: row[5],
+      // Default value yang dibutuhkan aplikasi
+      reminderMinutes: 15,
+      isAlarmActive: true,
+      updatedAt: new Date().toISOString(),
+    };
+
+    // Baru simpan objeknya
     store.add(item);
   });
 
@@ -81,8 +97,11 @@ function saveImportedData(dataList) {
     alert(`Berhasil mengimpor ${dataList.length} jadwal!`);
     window.location.href = "index.html";
   };
-}
 
+  transaction.onerror = (err) => {
+    console.error("Gagal simpan data scan:", err);
+  };
+}
 // Jalankan Kamera saat tab "Terima Data" diklik
 document
   .getElementById("pills-receive-tab")
